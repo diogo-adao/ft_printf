6a6d523e27ac741840d05deee065aada236a34be
diogo-adao <diogoadao01x@gmail.com>
2024-05-03 20:03:22 +0100

    starting printf

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diolivei <diolivei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 17:49:37 by diolivei          #+#    #+#             */
+/*   Updated: 2024/05/03 18:01:31 by diolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+
+#endif
